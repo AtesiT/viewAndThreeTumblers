f1a589e9ee163496db68a431ec9ba64b33ef0ebe
AtesiT <atesit@mail.ru>
2025-08-18 22:04:07 +0300

    add stack view and init sliders

--- viewAndThreeTumblers/ViewController.swift
+++ viewAndThreeTumblers/ViewController.swift
@@ -1,19 +1,54 @@
-//
-//  ViewController.swift
-//  viewAndThreeTumblers
-//
-//  Created by Yarutov on 10.08.2025.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var redLabel: UILabel!
+    @IBOutlet var greenLabel: UILabel!
+    @IBOutlet var blueLabel: UILabel!
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
+    @IBOutlet var colorView: UIView!
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        initialAndRangeOfTumblers()
+    }
+    
+    private func initialAndRangeOfTumblers() {
+        redSlider.value = 0
+        greenSlider.value = 0
+        blueSlider.value = 0
+        
+        redSlider.minimumValue = 0
+        greenSlider.minimumValue = 0
+        blueSlider.minimumValue = 0
+        
+        redSlider.maximumValue = 255
+        greenSlider.maximumValue = 255
+        blueSlider.maximumValue = 255
+        
+        redSlider.minimumTrackTintColor = .red
+        greenSlider.minimumTrackTintColor = .green
+        blueSlider.minimumTrackTintColor = .blue
     }
 
-
+    @IBAction func redSliderAction() {
+        
+    }
+    
+    @IBAction func greenSliderAction() {
+    }
+    
+    @IBAction func blueSliderAction() {
+    }
+    @IBAction func changeColor() {
+        
+    }
+    
 }
 
